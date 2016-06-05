2990650b457364e9afa4810bacb3a91a98b22f3e
thr3a <12jk172@ms.dendai.ac.jp>
2016-06-06 01:48:11 +0900

    グラフのオプションを個別ファイルに移動した

---
+++ app/assets/javascripts/graph.js
@@ -0,0 +1,33 @@
+var graph_option = {
+  chart: {
+    renderTo: 'graph'
+  },
+  title: {
+    text: '',
+    x: -20 //center
+  },
+  subtitle: {
+    text: '',
+    x: -20
+  },
+  xAxis: {
+    categories: []
+  },
+  yAxis: [{
+    title: {text: '気温'}
+  },{
+    title: {text:'湿度'},opposite: true
+  }],
+  plotLines: [{
+    value: 0,
+    width: 1,
+    color: '#808080'
+  }],
+  legend: {
+    layout: 'vertical',
+    align: 'right',
+    verticalAlign: 'middle',
+    borderWidth: 0
+  },
+  series: []
+};
